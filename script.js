3d3df15b91bbb7a1ec2f8970fec5563817aa21ce
erjona00 <119108914+erjona00@users.noreply.github.com>
2022-12-22 00:53:27 +0100

    updated

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-function validimi(){
-    var emriUserit = document.getElementById(f.name).value;
-    console.log(emriUserit);
-    var emri2userit = document.getAnimations(l.name).value;
-    console.log(emri2Userit);
-    var emailUserit = document.getAnimations(email).value;
-    console.log(emailUserit);
-    var passUserit = document.getAnimations(pass).value;
-    console.log(passUserit);
-    var pass2Userit = document.getAnimations(c.pass).value;
-    console.log(pass2userit);
-    var telUserit = document.getAnimations(number).value;
-    console.log(telUserit);
+var varguImazheve=[
+    'img1.jpg',
+    'img2.jpg',
+    'img3.jpg'
+];
 
-    if(emriUserit == ""){
-        alert("Please fill in this field ! ")
-    }
-    if(emri2Userit == ""){
-        console.log("Please fill in this field ! ")
-    }
-    if(emailUserit == ""){
-        console.log("Please fill in this field !")
-    }
-    if(pass2Userit == ""){
-        console.log("Please fill in this field ! ")
-    }
-    if(telUserit == ""){
-        console.log("Please fill in this field ! ")
-    } else{
-        alert("Regjistrimi eshte kryer me sukses.")
-    }
+var index = 0;
+const koha = 1500;
 
+function krijoSlider(){
+    document.getElementById('slider').src = varguImazheve[index];
+    index++;
+
+    if(index == varguImazheve.length){
+        index = 0;
+    }
+    setTimeout('krijoSlider()', koha);
 }
+krijoSlider();
+
+var varguImazheve1=[
+    'makeup1.jpg',
+    'makeup2.jpg'
+];
+var ind = 0;
+const k = 1500;
+
+function krijoSlider2(){
+    document.getElementById('slider1').src = varguImazheve1[ind];
+    ind++;
+
+    if(ind == varguImazheve.length){
+        ind = 0;
+    }
+    setTimeout('krijoSlider2()', k);
+}
+krijoSlider2();
